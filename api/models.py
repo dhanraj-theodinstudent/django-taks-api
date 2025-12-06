dbf46b2123676de53584c65ee5b386a4a3d19c76
dhanraj-theodinstudent <221956997+dhanraj-theodinstudent@users.noreply.github.com>
2025-12-07 01:48:04 +0530

    Initial commit - Django API setup

---
+++ api/models.py
@@ -0,0 +1,9 @@
+# api/models.py
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return self.title
