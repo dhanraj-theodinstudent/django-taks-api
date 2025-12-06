4f57ca797cde2ae8fbd9539aeaa9e2569ba5e084
dhanraj-theodinstudent <221956997+dhanraj-theodinstudent@users.noreply.github.com>
2025-12-07 02:39:39 +0530

    Added PostgreSQL support

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,12 +78,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# core/settings.py
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# This attempts to read the DATABASE_URL environment variable.
+# If it exists (Production), it overrides the default SQLite configuration.
+database_url = os.environ.get("DATABASE_URL")
+if database_url:
+    DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 # Password validation
